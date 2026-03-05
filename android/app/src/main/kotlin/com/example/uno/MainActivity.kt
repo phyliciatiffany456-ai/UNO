5576e0110a5882969d81a964f3ef88189865dd40
phyliciatiffany456-ai <phyliciatiffany456@gmail.com>
2026-03-05 23:29:07 +0700

    Template App

---
+++ android/app/src/main/kotlin/com/example/uno/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uno
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ unoapp/android/app/src/main/kotlin/com/example/unoapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.unoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
